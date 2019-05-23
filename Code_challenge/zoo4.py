6120e0bdd4a04e02fab3e9bb036d00bf04a9316c
Krishna Kumar Parasrampuria <41633551+krishppuria@users.noreply.github.com>
2019-05-23 23:09:19 +0530

    changes some variable names

--- Code_challenge/zoo4.py
+++ Code_challenge/zoo4.py
@@ -3,13 +3,13 @@ while (True):
     try:
         file = open('C:/Users/Krishna/Desktop/Python work/Files/zoo.csv', 'r')
         itter_list=file.readlines()
-        sum=0
+        water_sum=0
         #animals_dict={'elephant':0,'zebra':0,'lion':0,'kangaroo':0,'tiger':0}
         for lines in itter_list:
             new_list=lines.split(",")
             if new_list[2]!='water_need\n':
-                sum+=int(new_list[2])
-        print("Total water needed:",sum)
+                water_sum+=int(new_list[2])
+        print("Total water needed:",water_sum)
     except Exception as e:
         print("Exception Occur: ", e)
     else:
